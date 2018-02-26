4ffac8daad9adad4d8df638f75846e034a35e5d4
Eoghan Hynes <eoghan@hyn.es>
2018-02-26 23:55:03 +0000

    Pentatonic scale

--- src/synth.js
+++ src/synth.js
@@ -1,3 +1,24 @@
-import _Tone from 'tone'
+import Tone from 'tone'
 
-export default new _Tone.PolySynth(16, _Tone.Synth).toMaster();
+const Synth = new Tone.PolySynth(16, Tone.Synth).toMaster();
+
+Synth.pentatonic = [
+  'A5',
+  'G5',
+  'E5',
+  'D5',
+  'C5',
+  'A4',
+  'G4',
+  'E4',
+  'D4',
+  'C4',
+  'A3',
+  'G3',
+  'E3',
+  'D3',
+  'C3',
+  'A2',
+];
+
+export default Synth;
