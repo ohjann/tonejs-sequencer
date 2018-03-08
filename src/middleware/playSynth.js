fac97d986f6977792db7930d84ec55ca118fc335
Eoghan Hynes <eoghan@thejournal.ie>
2018-03-08 18:58:06 +0000

    Small performance tweak

--- src/middleware/playSynth.js
+++ src/middleware/playSynth.js
@@ -6,7 +6,7 @@ const playSynth = store => next => action => {
     for (let row = 0; row < state.matrix.length; row++) {
       let column = state.activeColumn === state.matrix[row].length - 1 ? 0 : state.activeColumn + 1;
       if (state.matrix[row][column] === 1) {
-        synth.triggerAttackRelease( synth.pentatonic[row], "8n");
+        synth.triggerAttackRelease( synth.pentatonic[row], "8n", "+0.05");
       }
     }
   }
