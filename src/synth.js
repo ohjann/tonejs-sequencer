d5fb346431b1c65836ff2b6f5402580161a631eb
Eoghan Hynes <eoghan@hyn.es>
2018-06-19 21:49:20 +0100

    Add backing

--- src/synth.js
+++ src/synth.js
@@ -1,7 +1,10 @@
-import Tone from 'tone'
+import Tone from 'tone';
+import * as MidiConvert from 'midiconvert'
 
 const Synth = new Tone.PolySynth(8);
+const Synth2 = new Tone.PolySynth(8);
 
+const feedbackDelay = new Tone.FeedbackDelay("4n", 0.6).toMaster();
 Synth.set({
   'oscillator' : {
     'type' : 'triangle8'
@@ -14,7 +17,21 @@ Synth.set({
   }
 });
 
+Synth2.set({
+  'oscillator' : {
+    'type' : 'sine'
+  },
+  'envelope' : {
+    'attack' : 0,
+    'decay' : 3,
+    'sustain': 0.2,
+    'release': 2
+  }
+});
+
 Synth.toMaster();
+Synth2.connect(feedbackDelay);
+Synth2.toMaster();
 
 Synth.pentatonic = [
   'A5',
@@ -34,5 +51,26 @@ Synth.pentatonic = [
   'C3',
   'A2',
 ];
+
+MidiConvert.load("/backing.mid", function(midi) {
+
+  // make sure you set the tempo before you schedule the events
+  Tone.Transport.bpm.value = midi.header.bpm / 2;
+
+  // pass in the note events from one of the tracks as the second argument to Tone.Part
+  var midiPart = new Tone.Part(function(time, note) {
+
+    //use the events to play the synth
+    Synth2.triggerAttackRelease(note.name, note.duration, time, note.velocity / 1.2)
+
+  }, midi.tracks[0].notes).start()
+
+  midiPart.loopStart = 0;
+  midiPart.loopEnd = 32;
+  midiPart.loop = true;
+
+  // start the transport to hear the events
+  Tone.Transport.start()
+})
 
 export default Synth;
